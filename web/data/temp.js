71da1c09d218215c93c0dd4bde11565adb364f66
Joshua Gracie <joshua_gracie@aol.com>
2019-10-15 15:17:28 -0400

    Added template to account page
    Added template look to account page
    Added div to index.html for fixed scrolling (change made in accounts.html caused original overflow-y:scroll to break)

--- web/data/temp.js
+++ web/data/temp.js
@@ -47,3 +47,47 @@ function MenuFunction(x) {
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
 }
+
+function openPage(pageName, elmnt, btnName) {
+  // Hide all elements with class="tabcontent" by default */
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  btnOpen = document.getElementsByClassName("AcPageBtn");
+  for (i = 0; i < btnOpen.length; i++) {
+    btnOpen[i].style.background = "inherit";
+    btnOpen[i].style.color = "white";
+
+  }
+
+
+  // Show the specific tab content
+  document.getElementById(pageName).style.display = "block";
+  document.getElementById(btnName).style.background = "white";
+  document.getElementById(btnName).style.color = "black";
+
+  var x = document.querySelector(".accsidebar");
+  var y = document.querySelector(".acc-content-pane");
+  x.style.height = y.style.height
+
+}
+
+// Get the element with id="defaultOpen" and click on it
+window.onload = function() {
+  document.getElementById("defaultOpen").click();
+
+};
+
+function AccMenuFunction(x) {
+  document.getElementById("accsidebar").style.width = "100%";
+  document.getElementById("accsidebar").style.position = "fixed";
+}
+
+function acccloseNav() {
+  document.getElementById("accsidebar").style.width = "0";
+  var y = document.querySelector(".acc-content-pane");
+  y.style.width("auto")
+}
