bba6ced5dc01e134e7c3eaa1114951e08b4c816c
E1den <andrew.d.kiner@gmail.com>
2019-10-21 22:15:28 -0400

    Remade the comic searching and first half of password reset

--- node/data/accounts.js
+++ node/data/accounts.js
@@ -1,6 +1,8 @@
 //Handles logging in
 const mysql = require('mysql');
 const config = require('./config.js');
+const mailer = require('nodemailer');
+const fs = require('fs');
 
 //Connection data, only used when connect called
 const con = mysql.createConnection({
@@ -8,16 +10,52 @@ const con = mysql.createConnection({
     user: config.meta.credentials.user,
     password: config.meta.credentials.password,
     database: config.meta.credentials.database
-    });
+});
+
+const sender = mailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: config.meta.mail.user,
+        pass: config.meta.mail.password
+    }
+});
+
+var mail = {
+    from: config.meta.mail.user,
+    to: config.meta.mail.user,
+    subject: "Password Reset Instructions",
+    attachments: [{
+        filename: 'logo.png',
+        path: 'static_pages/logo.png',
+        cid: 'logo'
+    },
+    {
+        filename: 'reset.png',
+        path: 'static_pages/reset.png',
+        cid: 'reset'
+    }],
+    html: "__ERROR__"
+};
+
+fs.readFile('static_pages/password.html', 'utf8', function (err, data) {
+    mail.html = data;
+});
 
 //Handles user login
-exports.login = function(req, res) {
-    const email = req.body[0].value;
-    const password = req.body[1].value;
+exports.login = function (req, res) {
+    try {
+        email = req.body[0].value;
+        password = req.body[1].value;
+    }
+    catch (e) {
+        req.query.e = 400;
+        err.error(req, res);
+        return;
+    }
+
 
     //quick validate, html, and function check
-    if(email==""|email.includes("<")|email.includes(")")|email.includes("'")|email.includes("\"")|!email.includes("@")|!email.includes("."))
-    {
+    if (email == "" | email.includes("<") | email.includes(")") | email.includes("'") | email.includes("\"") | !email.includes("@") | !email.includes(".")) {
         res.send("failure");
         return;
     }
@@ -33,20 +71,26 @@ exports.login = function(req, res) {
 }
 
 //Handles user logout
-exports.logout = function(req,res) {
+exports.logout = function (req, res) {
     delete req.session.user;
     res.send("done");
 }
 
 //Handles user signing up for a new account
-exports.signup = function(req, res) {
-    const user = req.body[0].value;
-    const email = req.body[1].value;
-    const password = req.body[2].value;
+exports.signup = function (req, res) {
+    try {
+        user = req.body[0].value;
+        email = req.body[1].value;
+        password = req.body[2].value;
+    }
+    catch (e) {
+        req.query.e = 400;
+        err.error(req, res);
+        return;
+    }
 
     //quick validate, html, and function check
-    if(email==""|email.includes("<")|email.includes(")")|email.includes("'")|email.includes("\"")|!email.includes("@")|!email.includes(".")|user==""|user.includes("<")|user.includes(")")|user.includes("'")|user.includes("\""))
-    {
+    if (email == "" | email.includes("<") | email.includes(")") | email.includes("'") | email.includes("\"") | !email.includes("@") | !email.includes(".") | user == "" | user.includes("<") | user.includes(")") | user.includes("'") | user.includes("\"")) {
         res.send("failure");
         return;
     }
@@ -61,21 +105,66 @@ exports.signup = function(req, res) {
     res.send("success");
 }
 
+exports.forgot = function (req, res) {
+    try {
+        email = req.body[0].value;
+    }
+    catch (e) {
+        req.query.e = 400;
+        err.error(req, res);
+        return;
+    }
+
+    //quick validate, html, and function check
+    if (email == "" | email.includes("<") | email.includes(")") | email.includes("'") | email.includes("\"") | !email.includes("@") | !email.includes(".")) {
+        res.send("failure");
+        return;
+    }
+
+    //TODO
+
+    //replace __USERNAME__
+    //replace __SERVER__
+    //replace __RESETKEY__
+
+    key = "RANDOM_HASHEDKEY_FOR_A_TEMP_AMMOUNT_OF_TIME"
+
+    user = "username"
+    path = "/srv/acct/forgot?k="+key
+
+    pack = mail;
+    pack.to = email;
+    pack.html = pack.html.replace(/__USERNAME__/g, user)
+    pack.html = pack.html.replace(/__SERVER__/g, config.meta.server.public)
+    pack.html = pack.html.replace(/__RESETKEY__/g, path);
+
+    sender.sendMail(pack);
+
+    //always send success to obscure account existance
+    res.send("success")
+}
+
+exports.updateForgot = function(req, res) {
+    key = req.query.k
+    //validate key exists,
+    //send password update page in this function
+    //that page sends data to the update function in a POST request
+    //that then logs the user in and forwards back to the homepage
+}
+
 //Handles user updating data, eg password
-exports.update = function(req, res) {
+exports.update = function (req, res) {
     //TODO
 }
 
 //Gets the current data to display about login state for a given user
-exports.getAccountSession = function(req, res) {
+exports.getAccountSession = function (req, res) {
     //into: "account-session-on"
 
-    if(req.session.user)
-    {
-        res.send("<a href='/accounts' class='nav-link' id='AccName'>"+req.session.user+"</a><button onclick='signOut()' class='sign-out-btn'><c>Sign Out</c></button>");
+    if (req.session.user) {
+        res.send("<a href='/accounts' class='nav-link' id='AccName'>" + req.session.user + "</a><button onclick='signOut()' class='sign-out-btn'><c>Sign Out</c></button>");
     }
-    else
-    {
+    else {
         res.send("<a href='/login' class='nav-link'>Login/Signup</a>");
     }
 }
--- web/data/gallery.js
+++ web/data/gallery.js
@@ -5,22 +5,38 @@ var full = false;
 const startTime = new Date();
 function grabNextRows() {
     var data = {
-        'row':rowNumber+=5,
-        'start':startTime.toJSON()
+        search: 0,
+        type: 0,
+        row: rowNumber += 5,
+        start: startTime.toJSON()
     };
     $.ajax({
         type: "POST",
         datatype: 'json',
-        url: "/srv/comic/gallery.js",
+        url: "/srv/comic/search",
         contentType: 'application/json',
         data: JSON.stringify(data),
-        success: function (n) { if(n=="done") {full=true; return;}; $(".comic-gallery").append(n); }
+        success: function (n) { if (n == "done") { full = true; return; } else { full = false; } $(".comic-gallery").append(n); }
     });
 };
+
+function pollForComics(n) {
+    $.ajax({
+        type: "POST",
+        datatype: 'json',
+        url: "/srv/comic/search",
+        contentType: 'application/json',
+        data: JSON.stringify({ search: n, type: 1, row: 0, start: (new Date().toJSON()) }),
+        success: function (j) { if (n == "done") { full = true; return; } else { full = false; } $(".comic-gallery").html(j); $(".content-pane").scrollTop(0);}
+    })
+}
+
+
 grabNextRows();
-$(document).ready(function() {
-    $(".content-pane").scroll(function(){
-        if(!full && $(".content-pane").scrollTop() + window.innerHeight >= $(".comic-gallery").height() - 5 * $(".book").height())
-           grabNextRows();
+$(document).ready(function () {
+    $(".content-pane").scroll(function () {
+        if (!full && $(".content-pane").scrollTop() + window.innerHeight >= $(".comic-gallery").height() - 5 * $(".book").height())
+            grabNextRows();
     });
+    $(".search-bar form").submit(function () { pollForComics($(".seach-input")[0].value) })
 });
