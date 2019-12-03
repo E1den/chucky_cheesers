dd11ea5f90a893a487734d127ff616b1a2f1df7c
E1den <andrew.d.kiner@gmail.com>
2019-12-02 20:27:37 -0500

    ?

--- node/data/comic.js
+++ node/data/comic.js
@@ -122,8 +122,8 @@ exports.getPages = function (req, res) {
     mysql.accessComicPageList(req.body.id, function (err, rows) {
         rows.forEach(function (row,index) {
             mysql.accessPage(row.page_id,function(err,row){
-                console.log(JSON.parse(row.layout));
-                data.page.push(JSON.parse(row.layout));
+                console.log(row.layout);
+                data.page.push(row.layout);
                 if(index==rows.length-1)
                 {
                     console.log(data);
